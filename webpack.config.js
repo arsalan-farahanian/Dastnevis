a2c7f00864f90c06dad9a3c35eae1cf612f92187
Arsalan Farahanian <amirarsalanfarahanian@gmail.com>
2022-02-04 21:18:22 +0330

    Client-Side structure was established

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  watch: true,
+  devtool: "cheap_module_eval_source_map",
+  entry: "./public/assets/js/index.js",
+  output: {
+    filename: "application.js",
+    path: path.resolve(__dirname, "public", "build", "js"),
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|svg|woff2?|eot|ttf|otf|wav)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "../../img/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../css/application.css",
+    }),
+  ],
+};
