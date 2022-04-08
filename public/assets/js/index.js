99c70924c3dfd497bd67fdf4506d309d811aa67f
Arsalan Farahanian <amirarsalanfarahanian@gmail.com>
2022-04-08 19:33:32 +0430

    signup page developed

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,6 +2,7 @@ import "../css/fonts.css";
 
 import "mdbootstrap/css/bootstrap.css";
 import "mdbootstrap/css/mdb.css";
+
 import "@fortawesome/fontawesome-free/js/brands";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/fontawesome";
