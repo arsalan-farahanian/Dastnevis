e5a270159bc71abcec398bf0e24643e824cd9757
Arsalan Farahanian <amirarsalanfarahanian@gmail.com>
2022-05-04 17:30:19 +0430

    fontawesome 5 is added to the project

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,12 @@
-import "../css/fonts.css";
-
 import "mdbootstrap/css/bootstrap.css";
 import "mdbootstrap/css/mdb.css";
+import "../css/fonts.css";
 
-import "@fortawesome/fontawesome-free/js/brands";
-import "@fortawesome/fontawesome-free/js/solid";
-import "@fortawesome/fontawesome-free/js/fontawesome";
+// import "@fortawesome/fontawesome-free/js/brands";
+// import "@fortawesome/fontawesome-free/js/solid";
+// import "@fortawesome/fontawesome-free/js/fontawesome";
+
+import "../css/all.min.css";
+import "../css/fontawesome.min.css";
 
 import "../css/style.css";
