ad161f2ccc8281f4c9016a9c82e3300f6c5e8cb2
Arsalan Farahanian <amirarsalanfarahanian@gmail.com>
2022-05-04 20:38:16 +0430

    Mongoose is added to the project

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,10 @@ const conAuth = require("../controllers/auth");
 
 module.exports = () => {
   router.get("/", conIndex.get_homepage);
+
+  //auth
   router.get("/login", conAuth.get_login);
   router.get("/signup", conAuth.get_signup);
+
   return router;
 };
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
+const { PORT, MONGODB_URI } = require("./config");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./routes/routes")());
-
-app.listen(PORT, () => {
-  console.log(`App server running on PORT ${PORT}`);
-});
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGODB CONNECTION ESTABLISHED");
+    app.listen(PORT, () => {
+      console.log(`App server running on PORT ${PORT}`);
+      app.use(require("./routes/routes")());
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    app.listen(PORT, () => {
+      app.use((req, res) => {
+        res.send(
+          '<p class="alert aler-danger h3 text-center">خطا در برقراری اتصال به پایگاه داده</p>'
+        );
+      });
+    });
+  });
